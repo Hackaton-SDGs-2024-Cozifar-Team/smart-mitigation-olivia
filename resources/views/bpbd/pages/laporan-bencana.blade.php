927e8263ed78a461a4c2a7fcd4cb83e778494d0c
farhanariyanto03 <ariyantofarhan03@gmail.com>
2025-07-22 09:26:51 +0700

    Fix: error logic admin & bpbd

--- resources/views/bpbd/pages/laporan-bencana.blade.php
+++ resources/views/bpbd/pages/laporan-bencana.blade.php
@@ -44,8 +44,9 @@
                                             <i class="ri-close-fill"></i>
                                         </button>
                                     </form>
-                                    <button type="button" data-modal-target="authentication-modal"
-                                        data-modal-toggle="authentication-modal"
+                                    <button type="button"
+                                        data-modal-target="modal-detail-{{ $bencana->id_laporan_bencana }}"
+                                        data-modal-toggle="modal-detail-{{ $bencana->id_laporan_bencana }}"
                                         class="text-yellow-400 px-4 hover:text-white border border-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:outline-none focus:ring-yellow-300 rounded-lg p-2">
                                         <i class="ri-more-fill"></i>
                                     </button>
@@ -56,29 +57,31 @@
                 </table>
             </div>
 
-            <!-- Main modal -->
-            <div id="authentication-modal" tabindex="-1" aria-hidden="true"
-                class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
-                <div class="relative p-4 w-full max-w-md max-h-full">
-                    <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
-                        <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
-                            <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
-                                Detail Laporan Bencana
-                            </h3>
-                            <button type="button"
-                                class="end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
-                                data-modal-hide="authentication-modal">
-                                <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
-                                    viewBox="0 0 14 14">
-                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-                                        stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
-                                </svg>
-                                <span class="sr-only">Close modal</span>
-                            </button>
-                        </div>
-                        <!-- Modal body -->
-                        <div class="p-4 md:p-5">
-                            @foreach ($laporan_bencana as $laporan)
+            <!-- Modal Section -->
+            @foreach ($laporan_bencana as $laporan)
+                <div id="modal-detail-{{ $laporan->id_laporan_bencana }}" tabindex="-1" aria-hidden="true"
+                    class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+                    <div class="relative p-4 w-full max-w-md max-h-full">
+                        <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                            <div
+                                class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
+                                <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
+                                    Detail Laporan Bencana
+                                </h3>
+                                <button type="button"
+                                    class="end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+                                    data-modal-hide="modal-detail-{{ $laporan->id_laporan_bencana }}">
+                                    <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                                        fill="none" viewBox="0 0 14 14">
+                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                            stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+                                    </svg>
+                                    <span class="sr-only">Close modal</span>
+                                </button>
+                            </div>
+
+                            <!-- Modal Body -->
+                            <div class="p-4 md:p-5">
                                 <div class="mb-3">
                                     <span class="block text-md font-medium text-gray-900 dark:text-gray-200">Nama
                                         Pelapor:</span>
@@ -87,7 +90,8 @@
                                 <div class="mb-3">
                                     <span class="block text-md font-medium text-gray-900 dark:text-gray-200">Tanggal
                                         Kejadian:</span>
-                                    <p class="text-sm text-gray-700 dark:text-gray-400">{{ $laporan->tanggal_kejadian }}</p>
+                                    <p class="text-sm text-gray-700 dark:text-gray-400">{{ $laporan->tanggal_kejadian }}
+                                    </p>
                                 </div>
                                 <div class="mb-3">
                                     <span class="block text-md font-medium text-gray-900 dark:text-gray-200">Nama
@@ -108,18 +112,20 @@
                                 <div class="mb-3">
                                     <span class="block text-md font-medium text-gray-900 dark:text-gray-200">Desa
                                         Kejadian:</span>
-                                    <p class="text-sm text-gray-700 dark:text-gray-400">{{ $laporan->desa_bencana }}</p>
+                                    <p class="text-sm text-gray-700 dark:text-gray-400">{{ $laporan->desa->nama_desa }}</p>
                                 </div>
                                 <div class="mb-3">
                                     <span class="block text-md font-medium text-gray-900 dark:text-gray-200">Foto
                                         Bencana:</span>
-                                    <p class="text-sm text-gray-700 dark:text-gray-400">Tidak ada foto tersedia</p>
+                                    <img src="{{ asset("uploads/bencana/$laporan->foto_bencana") }}" alt="Foto Bencana"
+                                        class="w-full rounded">
                                 </div>
-                            @endforeach
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
+            @endforeach
+
         </div>
     </div>
 @endsection
